b8ba0076368d7b0225c8c7171e6c024d06799e55
JeffRisberg <jrisberg@aol.com>
2017-04-07 18:42:16 -0700

    added active header highlight, removed requirement for jQuery.

--- app/js/components/Header.js
+++ app/js/components/Header.js
@@ -13,24 +13,22 @@ class Header extends React.Component {
 
     render() {
         return (
-            <div>
-                <div className="navbar navbar-inverse navbar-fixed-top" style={{marginBottom: '0px'}}>
-                    <div className="container">
-                        <div className="navbar-inner">
-                            <NavLink to="/" className="navbar-brand" style={{padding: 0}}>
-                                <img src="/images/logo.jpg"/>
-                            </NavLink>
+            <nav className="navbar navbar-inverse navbar-fixed-top" style={{marginBottom: '0px'}}>
+                <div className="container">
+                    <div className="navbar-inner">
+                        <NavLink to="/" className="navbar-brand" style={{padding: 0}}>
+                            <img src="/images/logo.jpg"/>
+                        </NavLink>
 
-                            <div className="navbar-collapse collapse">
-                                <ul className="nav navbar-nav">
-                                    <li><NavLink to="/fixedDataTableExample">FixedDataTableExample</NavLink></li>
-                                    <li><NavLink to="/griddleExample">GriddleExample</NavLink></li>
-                                </ul>
-                            </div>
+                        <div className="navbar-collapse collapse">
+                            <ul className="nav navbar-nav">
+                                <li><NavLink to="/fixedDataTableExample">FixedDataTableExample</NavLink></li>
+                                <li><NavLink to="/griddleExample">GriddleExample</NavLink></li>
+                            </ul>
                         </div>
                     </div>
                 </div>
-            </div>
+            </nav>
         )
     }
 }
