ae3f057e89e3f263bffdbe248fd17bdc10b19588
JeffRisberg <jrisberg@aol.com>
2017-09-19 16:53:09 -0700

    added notification example

--- app/js/components/Header.js
+++ app/js/components/Header.js
@@ -1,7 +1,8 @@
-import React from "react";
-import {Link} from "react-router";
-import {connect} from "react-redux";
-import NavLink from "./NavLink";
+import React from 'react';
+import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import NavLink from './NavLink';
+import Dropdown, { DropdownTrigger, DropdownContent } from 'react-simple-dropdown';
 
 /**
  * Appears at top of screen
@@ -11,36 +12,88 @@ import NavLink from "./NavLink";
  */
 class Header extends React.Component {
 
-    render() {
-        return (
-            <nav className="navbar navbar-inverse navbar-fixed-top" style={{marginBottom: '0px'}}>
-                <div className="container">
-                    <div className="navbar-inner">
-                        <NavLink to="/" className="navbar-brand" style={{padding: 0}}>
-                            <img src="/images/logo.jpg"/>
-                        </NavLink>
+  constructor(props) {
+    super(props);
 
-                        <div className="navbar-collapse collapse">
-                            <ul className="nav navbar-nav">
-                                <li><NavLink to="/fixedDataTableExample">FixedDataTableExample</NavLink></li>
-                                <li><NavLink to="/griddleExample">GriddleExample</NavLink></li>
-                                <li><NavLink to="/reactBootstrapExample">React Bootstrap Example</NavLink></li>
-                            </ul>
+    this.handleLinkClick = this.handleLinkClick.bind(this);
+  }
+
+  handleLinkClick() {
+    this.refs.dropdown.hide();
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-inverse navbar-fixed-top" style={{ marginBottom: '0px' }}>
+        <div className="container">
+          <div className="navbar-inner">
+            <NavLink to="/" className="navbar-brand" style={{ padding: 0 }}>
+              <img src="/images/logo.jpg"/>
+            </NavLink>
+
+            <div className="navbar-collapse collapse">
+              <ul className="nav navbar-nav">
+                <li><NavLink to="/fixedDataTableExample">FixedDataTableExample</NavLink></li>
+                <li><NavLink to="/griddleExample">GriddleExample</NavLink></li>
+                <li><NavLink to="/reactBootstrapExample">React Bootstrap Example</NavLink></li>
+              </ul>
+              <ul style={{ height: '50px' }} className="nav navbar-nav navbar-right">
+                <li style={{ height: '50px' }}>
+                  <Dropdown className="account-dropdown" ref="dropdown" style={{ height: '50px' }}>
+                    <DropdownTrigger
+                      style={{ float: 'right', height: '50px', textDecoration: 'none', color: '#9D9D9D' }}>
+                      <br/>
+                      Notifications
+                    </DropdownTrigger>
+                    <DropdownContent style={{ width: '440px', top: '45px', position: 'relative' }}>
+                      <div>
+                        <div className="notification">
+                          <a href="#" onClick={this.handleLinkClick}>
+                            Cash minimum will not be met on 09/22/17
+                          </a>
                         </div>
-                    </div>
-                </div>
-            </nav>
-        )
-    }
+                          <div className="notificationFooter">
+                            <a href="#" className="notificationAction">Show Details</a>
+                            <a href="#" className="notificationAction">Dismiss</a>
+                          </div>
+                        <div className="notification">
+                          <a href="#" onClick={this.handleLinkClick}>
+                            Major uptick in late deliverables
+                          </a>
+                        </div>
+                        <div className="notificationFooter">
+                          <a href="#" className="notificationAction">Show Details</a>
+                          <a href="#" className="notificationAction">Dismiss</a>
+                        </div>
+                        <div className="notification">
+                          <a href="#" onClick={this.handleLinkClick}>
+                            Early degenerative change in gasket #RT-345W
+                          </a>
+                        </div>
+                        <div className="notificationFooter">
+                          <a href="#" className="notificationAction">Show Details</a>
+                          <a href="#" className="notificationAction">Dismiss</a>
+                        </div>
+                      </div>
+                    </DropdownContent>
+                  </Dropdown>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        donor: state.donor
-    };
+  return {
+    donor: state.donor
+  };
 };
 export default connect(
-    mapStateToProps,
-    {}
+  mapStateToProps,
+  {}
 )(Header);
 
